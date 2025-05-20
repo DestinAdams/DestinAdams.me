4801f01a9b3e51822a2411d5bfe5cb127c706bb5
Worryinghippo2 <dadams34@asu.edu>
2025-05-19 21:52:58 -0700

    Trouble Shooting Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
-import Body from "@/components/Body";
 import Projects from "@/components/Projects";
 import Background from "@/components/Background";
 import Skills from "@/components/Skills";
-import Timeline from "@/components/Timeline";
-import Intakeform from "@/components/Intakeform";
+
+
 import Footer from "@/components/Footer";
-import HourlyApp from "@/components/HourlyApp";
-import { BrowserRouter } from "react-router-dom";
 export default function Home() {
   return (
     <>
@@ -18,7 +15,6 @@ export default function Home() {
       <Header />
       <Projects />
       <Skills />
-
       <Footer />
     </>
   );
